ce2b701d6579b9e199958b45b1c0db2055b0bdc7
mohamed khattab <96209701+7khattab@users.noreply.github.com>
2025-07-19 16:19:36 +0300

    Moved map_public_ip_on_launch outside the tags block

--- lab-1/main.tf
+++ lab-1/main.tf
@@ -1,44 +1,40 @@
-
 # Configure the AWS Provider
-
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "terraform-user"
 }
 
-# configure the VPC 
-
+# Configure the VPC 
 resource "aws_vpc" "test" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "test"
   }
 }
 
-# configure the subnet
-
+# Configure the subnet
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.test.id
   cidr_block = "10.0.1.0/24"
+  
+  # Fixed: map_public_ip_on_launch should be outside tags
+  map_public_ip_on_launch = true
+  
   tags = {
-    Name = "public subnet"
-    map_public_ip_on_launch = true
+    Name = "public-subnet"
   }
 }
 
-# configure the internet gateway
-
+# Configure the internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.test.id
-
   tags = {
-    Name = "gw"
+    Name = "main-gw"
   }
 }
 
-# configure the route_tabel 
-
-resource "aws_route_table" "test" {
+# Configure the route table 
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.test.id
 
   route {
@@ -47,84 +43,21 @@ resource "aws_route_table" "test" {
   }
 
   tags = {
-    Name = "route"
+    Name = "public-rt"
   }
 }
 
-# configure aws route table association
-
-resource "aws_route_table_association" "a" {
+# Configure route table association
+resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.test.id
+  route_table_id = aws_route_table.public.id
 }
 
-# configure aws_instance
-
+# Configure EC2 instance
 resource "aws_instance" "terraform" {
-  ami           = "ami-09e6f87a47903347c"
+  ami           = "ami-09e6f87a47903347c"  
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-=======
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  profile = "terraform-user"
-}
-
-# configure the VPC 
-resource "aws_vpc" "test" {
-  cidr_block       = "10.0.0.0/16"
-  tags = {
-    Name = "test"
-  }
-}
-
-# configure the subnet
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.test.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "public subnet"
-    map_public_ip_on_launch = true
-  }
-}
-
-# configure the internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.test.id
-
-  tags = {
-    Name = "gw"
-  }
-}
-
-# configure the route_tabel 
-resource "aws_route_table" "test" {
-  vpc_id = aws_vpc.test.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "route"
-  }
-}
-
-# configure aws route table association
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.test.id
-}
-
-# configure aws_instance
-resource "aws_instance" "terraform" {
-  ami           = "ami-09e6f87a47903347c"
-  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id  # Added subnet association
 
   tags = {
     Name = "HelloWorld"
